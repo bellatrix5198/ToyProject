cae62bd811b0577a9d8437f3e252f9d1b4014558
ihagjun <ihagjun@218.38.137.28>
2020-06-26 15:04:42 +0900

    bot basic code

--- telegrambot/src/main/java/com/telegram/telegrambot/TelegrambotApplication.java
+++ telegrambot/src/main/java/com/telegram/telegrambot/TelegrambotApplication.java
@@ -2,11 +2,26 @@ package com.telegram.telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import com.telegram.telegrambot.service.Telegrambot;
 
 @SpringBootApplication
 public class TelegrambotApplication {
 
 	public static void main(String[] args) {
+
+		ApiContextInitializer.init();
+
+		TelegramBotsApi api = new TelegramBotsApi();
+
+		try {
+			api.registerBot(new Telegrambot());
+		} catch (TelegramApiException e) {
+
+		}
 		SpringApplication.run(TelegrambotApplication.class, args);
 	}
 
---
+++ telegrambot/src/main/java/com/telegram/telegrambot/service/Telegrambot.java
@@ -0,0 +1,33 @@
+package com.telegram.telegrambot.service;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Telegrambot extends TelegramLongPollingBot {
+
+	@Override
+	public void onUpdateReceived(Update update) {
+		SendMessage massage = new SendMessage();
+
+		massage.setChatId(update.getMessage().getChatId()).setText("hello telegram");
+
+		try {
+			execute(massage);
+		} catch (TelegramApiException e) {
+
+		}
+	}
+
+	@Override
+	public String getBotUsername() {
+		return "Test_5198bot";
+	}
+
+	@Override
+	public String getBotToken() {
+		return "1171338273:AAHEKyWp4jGzgx1dBqFWe2Cga3MP6LhuhIk";
+	}
+
+}
